510921d4e19afc0600222f437dc114bac327808a
VitorComin <vitorgabrielcomin@gmail.com>
2023-11-14 11:59:37 -0300

    Final Commit (v1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,13 @@
 import Image from "next/image";
-import {
-  Home as HomeIcon,
-  Search,
-  Library,
-  ChevronLeft,
-  ChevronRight,
-} from "lucide-react";
+import { ChevronLeft, ChevronRight, Play } from "lucide-react";
+import { Sidebar } from "@/app/components/Sidebar.";
+import { Footer } from "@/app/components/Footer";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded-full" />
-            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
-            <div className="w-3 h-3 bg-green-500 rounded-full" />
-          </div>
-          <nav className="space-y-5 mt-10">
-            <a
-              href=""
-              className="flex items-center gap-3 text-zinc-200"
-              text-sn
-              font-semibold
-            >
-              <HomeIcon />
-              Home
-            </a>
-            <a
-              href=""
-              className="flex items-center gap-3 text-zinc-200"
-              text-sn
-              font-semibold
-            >
-              <Search />
-              Search
-            </a>
-            <a
-              href=""
-              className="flex items-center gap-3 text-zinc-200"
-              text-sn
-              font-semibold
-            >
-              <Library />
-              Your Library
-            </a>
-          </nav>
-          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Rock
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Sertanejo
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Eletrônica
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Esquenta
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Coding
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Modão
-            </a>
-          </nav>
-        </aside>
+        <Sidebar />
         <main className="flex-1 p-6">
           <div className="flex items-center gap-4">
             <button className="rounded-full bg-black/40 p-1">
@@ -80,7 +21,10 @@ export default function Home() {
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
 
           <div className="grid grid-cols-3 gap-4 mt-4">
-            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
               <Image
                 src="/8mile-album.jpeg"
                 width={104}
@@ -88,8 +32,14 @@ export default function Home() {
                 alt="Capa do álbum 8-mile do Eminem"
               />
               <strong>8-Mile</strong>
-            </div>
-            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <button className="w-10 h-10 flex items-center justify-center pl-2.5 p-1.5 rounded-full bg-green-400 text-black ml-auto mr-6 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
               <Image
                 src="/8mile-album.jpeg"
                 width={104}
@@ -97,8 +47,14 @@ export default function Home() {
                 alt="Capa do álbum 8-mile do Eminem"
               />
               <strong>8-Mile</strong>
-            </div>
-            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <button className="w-10 h-10 flex items-center justify-center pl-2.5 p-1.5 rounded-full bg-green-400 text-black ml-auto mr-6 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
               <Image
                 src="/8mile-album.jpeg"
                 width={104}
@@ -106,8 +62,14 @@ export default function Home() {
                 alt="Capa do álbum 8-mile do Eminem"
               />
               <strong>8-Mile</strong>
-            </div>
-            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <button className="w-10 h-10 flex items-center justify-center pl-2.5 p-1.5 rounded-full bg-green-400 text-black ml-auto mr-6 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
               <Image
                 src="/8mile-album.jpeg"
                 width={104}
@@ -115,8 +77,14 @@ export default function Home() {
                 alt="Capa do álbum 8-mile do Eminem"
               />
               <strong>8-Mile</strong>
-            </div>
-            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <button className="w-10 h-10 flex items-center justify-center pl-2.5 p-1.5 rounded-full bg-green-400 text-black ml-auto mr-6 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
               <Image
                 src="/8mile-album.jpeg"
                 width={104}
@@ -124,8 +92,14 @@ export default function Home() {
                 alt="Capa do álbum 8-mile do Eminem"
               />
               <strong>8-Mile</strong>
-            </div>
-            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <button className="w-10 h-10 flex items-center justify-center pl-2.5 p-1.5 rounded-full bg-green-400 text-black ml-auto mr-6 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
               <Image
                 src="/8mile-album.jpeg"
                 width={104}
@@ -133,13 +107,89 @@ export default function Home() {
                 alt="Capa do álbum 8-mile do Eminem"
               />
               <strong>8-Mile</strong>
-            </div>
+              <button className="w-10 h-10 flex items-center justify-center pl-2.5 p-1.5 rounded-full bg-green-400 text-black ml-auto mr-6 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">Made for you</h2>
+
+          <div className="grid grid-cols-5 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                src="/8mile-album.jpeg"
+                className="w-full"
+                width={104}
+                height={104}
+                alt="Capa do álbum 8-mile do Eminem"
+              />
+              <strong className="font-semibold">8-Mile</strong>
+              <span className="text-sm text-zinc-500">Eminem</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                src="/8mile-album.jpeg"
+                className="w-full"
+                width={104}
+                height={104}
+                alt="Capa do álbum 8-mile do Eminem"
+              />
+              <strong className="font-semibold">8-Mile</strong>
+              <span className="text-sm text-zinc-500">Eminem</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                src="/8mile-album.jpeg"
+                className="w-full"
+                width={104}
+                height={104}
+                alt="Capa do álbum 8-mile do Eminem"
+              />
+              <strong className="font-semibold">8-Mile</strong>
+              <span className="text-sm text-zinc-500">Eminem</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                src="/8mile-album.jpeg"
+                className="w-full"
+                width={104}
+                height={104}
+                alt="Capa do álbum 8-mile do Eminem"
+              />
+              <strong className="font-semibold">8-Mile</strong>
+              <span className="text-sm text-zinc-500">Eminem</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                src="/8mile-album.jpeg"
+                className="w-full"
+                width={104}
+                height={104}
+                alt="Capa do álbum 8-mile do Eminem"
+              />
+              <strong className="font-semibold">8-Mile</strong>
+              <span className="text-sm text-zinc-500">Eminem</span>
+            </a>
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
-      </footer>
+      <Footer />
     </div>
   );
 }
